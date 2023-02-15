46cfe0317d8289bdf3343749a3fe19bfe369d829
Andriw <filipefonsequinha@gmail.com>
2023-02-15 00:25:12 -0300

    alterado utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,8 @@
 (function (){
-    changeObjectNameJson();
+    transformJsonToArray();
 })();
 
-function changeObjectNameJson(){
+function transformJsonToArray(){
     const fs = require('fs');
 
     fs.readFile('src/data/data-champions.json', 'utf8', (err, json) => {
@@ -10,15 +10,9 @@ function changeObjectNameJson(){
     
       const {data} = JSON.parse(json);  
     
-      const championsByKey = {};
-    
-      for (const championName in data) {
-        const champion = data[championName];
-        const key = champion.key;
-        championsByKey[key] = champion;
-      }
-    
-      fs.writeFile('src/data/champions.json', JSON.stringify(championsByKey, null, 2), err => {
+      const champions = Object.values(data).map(champion => [champion]);  
+          
+      fs.writeFile('src/data/champions.json', JSON.stringify(champions, null, 2), err => {
         if (err) throw err;
         console.log('Arquivo modificado salvo!');
       });
